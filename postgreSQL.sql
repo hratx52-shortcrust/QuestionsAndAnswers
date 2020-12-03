af5c63bf4769bb73da7e72206d60d63ac8dfdf03
David Lichter <lichter14@gmail.com>
2020-12-03 17:16:34 -0600

    Added answers_photos to table. Has syntax error.

--- postgreSQL.sql
+++ postgreSQL.sql
@@ -21,3 +21,34 @@ CREATE TABLE questions
   product_id integer NOT NULL,
   CONSTRAINT questions_pkey PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS answers;
+
+CREATE TABLE answers
+(
+  id SERIAL,
+  reported integer NOT NULL,
+  date_written date NOT NULL,
+  body TEXT NOT NULL,
+  answerer_name TEXT NOT NULL,
+  answerer_email TEXT NOT NULL,
+  helpful integer NOT NULL,
+  question_id integer NOT NULL,
+  CONSTRAINT answers_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_question
+    FOREIGN KEY(question_id)
+      REFERENCES questions(id)
+);
+
+DROP TABLE IF EXISTS answers_photos;
+
+CREATE TABLE answers_photos
+(
+  id SERIAL,
+  answer_id integer NOT NULL,
+  url TEXT NOT NULL,
+  CONSTRAINT answers_photos_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_answers
+    FOREIGN KEY(answer_id)
+      REFERENCES answer(id)
+);
