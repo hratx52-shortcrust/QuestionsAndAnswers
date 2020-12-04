c269ebc4f6a3d552024c6a5c399415ee2ae0d289
David Lichter <lichter14@gmail.com>
2020-12-04 16:37:39 -0600

    Added skeleton for remaining routes

--- server/server.js
+++ server/server.js
@@ -7,12 +7,72 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
 
+//List Questions
 app.get('/qa/questions', (req, res) => {
-  productID = req.query.product_id;
+  let productID = req.query.product_id;
   var questionQuery = getQuestions(productID);
-})
 
+  //response status 200
+});
+
+//Answers List
 app.get('/qa/questions/:question_id/answers', (req, res) => {
-  questionID = req.params.question_id;
+  let questionID = req.params.question_id;
   var answerQuery = getAnswers(questionID);
-})
+
+  //response status 200
+});
+
+//Add a Question
+app.post('/qa/questions', (req, res) => {
+  // Body parameters: {
+  //   body,
+  //   name,
+  //   email,
+  //   product_id
+  // }
+
+  //response status 201
+});
+
+//Add an Answer
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+  let questionID = req.params.question_id;
+  // Body parameters: {
+  //   body,
+  //   name,
+  //   email,
+  //   photos
+  // }
+
+  //response status 201
+});
+
+//Mark Question as Helpful
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  let questionID = req.params.question_id;
+
+  //response status 204
+});
+
+//Report Question
+app.put('/qa/questions/:question_id/report', (req, res) => {
+  let questionID = req.params.question_id;
+
+  //response status 204
+});
+
+//Mark Answer as Helpful
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  let answerID = req.params.question_id;
+
+  //response status 204
+});
+
+//Report Answer
+app.put('/qa/questions/:answer_id/report', (req, res) => {
+  let answerID = req.params.question_id;
+
+  //response status 204
+});
+
